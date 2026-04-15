f6b9b00d2644cd90bcde3d3e661e8753a5a676d6
Aymanpg <aymanrahaman375@gmail.com>
2026-04-15 23:34:37 +0600

    setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Geist', 'system-ui', 'sans-serif']
+      },
+      colors: {
+        primary: '#244d3f',
+        accent: '#1a8862',
+        'text-dark': '#1f2937',
+        'text-body': '#64748b',
+        overdue: '#ef4444',
+        'almost-due': '#efad44',
+        'on-track': '#1a8862',
+        background: '#fafafa',
+      }
+    }
+  }
+}
